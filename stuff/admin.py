70dd345402c40e123b535a4d26b078ed5d9c255d
javadnr <javadnamavar554@gmail.com>
2023-09-18 19:44:20 +0330

    slug added

--- stuff/admin.py
+++ stuff/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Product, comment
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','id','price']
+    
 
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(comment)
 
--- stuff/models.py
+++ stuff/models.py
@@ -2,14 +2,19 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericRelation
+from django.utils.text import slugify
+import random
+from django.db.models.signals import pre_save, post_save
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=50,blank=True,null=True)
     discription = models.TextField()
     image = models.ImageField(upload_to='products/',null=True, blank = True, default='default.jpg')
     price =models.FloatField()
     date_added = models.DateTimeField(auto_now_add=True)
-
+    
     
     def __str__(self):
         return self.name
@@ -17,6 +22,44 @@ class Product(models.Model):
     def get_absolute_url(self):    
         return reverse("item_detail", args=[str(self.id)])
     
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+  
+
+
+
+def slugify_instance_name(instance, save=False, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.name)
+    Klass = instance.__class__
+    qs = Klass.objects.filter(slug=slug).exclude(id=instance.id)
+    if qs.exists():
+        
+        rand_int = random.randint(300_000, 500_000)
+        slug = f"{slug}-{rand_int}"
+        return slugify_instance_name(instance, save=save, new_slug=slug)
+    instance.slug = slug
+    if save:
+        instance.save()
+    return instance
+
+def product_pre_save(sender, instance, *args, **kwargs):
+    
+    if instance.slug is None:
+        slugify_instance_name(instance, save=False)
+
+pre_save.connect(product_pre_save, sender=Product)
+
+
+def product_post_save(sender, instance, created, *args, **kwargs):
+    
+    if created:
+        slugify_instance_name(instance, save=True)
+
+post_save.connect(product_post_save, sender=Product)
+
 
 
 class comment(models.Model):
