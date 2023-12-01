46721d13cc4aa1127e23fa6cf54bae878fd6b4e6
javadnr <javadnamavar554@gmail.com>
2023-12-01 14:18:25 +0330

    gitignore update

--- stuff/tests.py
+++ stuff/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Product,Comment
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class ProductTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create(username='john',email = 'john@email.com',password = '12345678')
+        cls.product = Product.objects.create(name = 'iphone16',price = '1000.00',discription = 'nice one')
+        cls.comment = Comment.objects.create(author = cls.user, post = cls.product, body = 'too expensive')
+        
+    def test_book_listing(self):
+        self.assertEqual(f"{self.product.name}", "iphone16")
+        self.assertEqual(f"{self.product.price}", "1000.00")
+        self.assertEqual(f"{self.product.discription}", "nice one")
+        
+    def test_book_list_view(self):
+        response = self.client.get(reverse("list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "iphone16")
+        self.assertTemplateUsed(response, "stuff/list.html")
+        
+    def test_book_detail_view(self):
+        response = self.client.get(self.product.get_absolute_url())
+        no_response = self.client.get("/detail/asidlasj/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "too expensive")
+        self.assertContains(response, "iphone16")
+        self.assertTemplateUsed(response, "stuff/detail.html")
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+from allauth.account.forms import SignupForm
+from allauth.account.views import SignupView
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+        username="will", email="will@email.com", password="testpass123"
+        )
+        self.assertEqual(user.username, "will")
+        self.assertEqual(user.email, "will@email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+        username="superadmin", email="superadmin@email.com", password="testpass123"
+        )
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+    
+class SignupPageTest(TestCase):
+    username = "newuser"
+    email = "newuser@email.com"
+    
+    def setUp(self):
+        url = reverse('account_signup')
+        self.response = self.client.get(url)
+        
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response,'account/signup.html')
+        self.assertContains(self.response, 'signup')
+        self.assertNotContains(self.response, 'hi!this is not in this page ')  
+              
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(self.username,self.email)
+        form = self.response.context.get('form')
+        self.assertEqual(get_user_model().objects.all().count(),1)
+        self.assertEqual(get_user_model().objects.all()[0].username,self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email,self.email)
+        
+        self.assertIsInstance(form, SignupForm)
+        self.assertContains(self.response ,"csrfmiddlewaretoken")
+        
+        
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignupView.as_view().__name__)
